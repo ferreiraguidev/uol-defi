7edb38ef1f2b903e2682503c9bb96b097260b520
Guilherme Ferreira <ferreiragui.dev@gmail.com>
2023-04-15 08:34:50 -0300

    uol defi begins

---
+++ src/main/java/com/example/uoldefi/UolDefiApplication.java
@@ -0,0 +1,13 @@
+package com.example.uoldefi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolDefiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UolDefiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uoldefi/UolDefiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uoldefi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolDefiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
